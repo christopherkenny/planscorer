a7a05d8680d7a93b2b60ae5b2d8ae725f96bf788
Christopher Kenny <ctk44@cornell.edu>
2022-11-22 23:39:26 -0500

    Initial commit

---
+++ R/ingest.R
@@ -0,0 +1,58 @@
+#' Ingest PlanScore Output
+#'
+#' @param link index url output from `ps_upload` functions
+#'
+#' @return tibble with district and plan level data
+#' @export
+#'
+#' @examples
+ps_ingest <- function(link) {
+
+  j <- jsonlite::read_json(link)
+
+  totals <- lapply(j$districts, \(x) purrr::pluck(x, 'totals')) |>
+    dplyr::bind_rows(.id = 'district') |>
+    dplyr::rename_with(
+      .fn = function(x)
+        x |>
+        tolower() |>
+        gsub(x = _, ' ', '_') |>
+        gsub(x = _, '\\+|,|\\-|\\)|\\(|\'', '') |>
+        gsub(x = _, '__', '_')
+    )
+
+  compactness <- lapply(j$districts, \(x) purrr::pluck(x, 'compactness')) |>
+    dplyr::bind_rows(.id = 'district') |>
+    dplyr::rename_with(
+      .fn = function(x)
+        x |>
+        tolower() |>
+        gsub(x = _, '-', '_')
+    )
+
+
+  incumbents <- tibble::enframe(j$incumbents) |>
+    tidyr::unnest_wider(value, names_repair = ~c('district', 'incumbents')) |>
+    suppressMessages()
+
+  summ <- j$summary |>
+    tibble::enframe() |>
+    tidyr::unnest('value') |>
+    tidyr::pivot_wider()|>
+    dplyr::rename_with(
+      .fn = function(x)
+        x |>
+        tolower() |>
+        gsub(x = _, ' ', '_') |>
+        gsub(x = _, '\\+', '') |>
+        gsub(x = _, '-', '_')
+    )
+
+  details <- Filter(function(x) length(x) == 1, j) |>
+    tibble::as_tibble()
+
+  dplyr::left_join(totals, compactness, by = 'district') |>
+    dplyr::bind_cols(
+      summ, details
+    )
+}
---
+++ R/upload.R
@@ -0,0 +1,77 @@
+#' Title
+#'
+#' @param file
+#' @param temporary Should a temporary PlanScore upload be used? Default is TRUE.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ps_upload_file <- function(file, temporary = TRUE) {
+
+  if (!is.logical(temporary)) {
+    cli::cli_abort('{.arg temporary} must be {.val TRUE} or {.val FALSE}.')
+  }
+
+  if (TRUE){#(fs::file_size(file) > 5e6) {
+    temporary <- FALSE
+    req <- httr2::request(base_url = api_url(temporary)) |>
+      httr2::req_auth_bearer_token(token = ps_get_key())
+    out <- req |>
+      httr2::req_perform() |>
+      httr2::resp_body_json()
+
+    req <- httr2::request(base_url = out[[1]]) |>
+      #httr2::req_auth_bearer_token(token = ps_get_key()) |>
+      httr2::req_body_multipart(
+        key = out[[2]]$key,
+        AWSAccessKeyId = out[[2]]$AWSAccessKeyId,
+        `x-amz-security-token` = out[[2]]$`x-amz-security-token`,
+        signature = out[[2]]$signature,
+        acl = out[[2]]$acl,
+        success_action_redirect = out[[2]]$success_action_redirect,
+        file = curl::form_file(file)
+      )
+
+    out <- req |>
+      httr2::req_perform() #>
+      #httr2::resp_body_json()
+
+  } else {
+    req <- httr2::request(base_url = api_url(temporary)) |>
+      httr2::req_auth_bearer_token(token = ps_get_key()) |>
+      httr2::req_body_file(path = file) # switch to req_body_multipart to add description
+
+    out <- req |>
+      httr2::req_perform() |>
+      httr2::resp_body_json()
+  }
+
+  out
+}
+
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ps_upload_redist <- function(map, plans, draw, temporary = TRUE) {
+
+
+
+  if (!is.logical(temporary)) {
+    cli::cli_abort('{.arg temporary} must be {.val TRUE} or {.val FALSE}.')
+  }
+
+  req <- httr2::request(base_url = api_url(temporary)) |>
+    httr2::req_auth_bearer_token(token = ps_get_key()) |>
+    httr2::req_body_file(path = file)
+
+  out <- req |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+
+  out
+}
