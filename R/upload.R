7a9c87a179dcad7949f11862ce9a78af4165e12b
Christopher Kenny <ctk44@cornell.edu>
2022-11-25 21:46:12 -0500

    up

--- R/upload.R
+++ R/upload.R
@@ -22,25 +22,26 @@ ps_upload_file <- function(file, temporary = TRUE) {
       httr2::resp_body_json()
 
     req <- httr2::request(base_url = out[[1]]) |>
-      #httr2::req_auth_bearer_token(token = ps_get_key()) |>
       httr2::req_body_multipart(
-        key = out[[2]]$key,
-        AWSAccessKeyId = out[[2]]$AWSAccessKeyId,
-        `x-amz-security-token` = out[[2]]$`x-amz-security-token`,
-        signature = out[[2]]$signature,
-        acl = out[[2]]$acl,
+        key                     = out[[2]]$key, #stringr::str_replace(out[[2]]$key, stringr::fixed('${filename}'), 'null-plan-incumbency.geojson'),
+        AWSAccessKeyId          = out[[2]]$AWSAccessKeyId,
+        `x-amz-security-token`  = out[[2]]$`x-amz-security-token`,
+        policy                  = out[[2]]$policy,
+        signature               = out[[2]]$signature,
+        acl                     = out[[2]]$acl,
         success_action_redirect = out[[2]]$success_action_redirect,
-        file = curl::form_file(file)
+        file                    = curl::form_file(file)
       )
 
     out <- req |>
       httr2::req_perform() #>
       #httr2::resp_body_json()
 
+
   } else {
     req <- httr2::request(base_url = api_url(temporary)) |>
       httr2::req_auth_bearer_token(token = ps_get_key()) |>
-      httr2::req_body_file(path = file) # switch to req_body_multipart to add description
+      httr2::req_body_file(path = file) # switch to req_body_multipart to add description?
 
     out <- req |>
       httr2::req_perform() |>
@@ -58,8 +59,6 @@ ps_upload_file <- function(file, temporary = TRUE) {
 #'
 #' @examples
 ps_upload_redist <- function(map, plans, draw, temporary = TRUE) {
-
-
 
   if (!is.logical(temporary)) {
     cli::cli_abort('{.arg temporary} must be {.val TRUE} or {.val FALSE}.')
