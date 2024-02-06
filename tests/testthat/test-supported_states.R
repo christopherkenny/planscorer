3049f6906693aaa30cfdb52151506a8edcc8b7ed
Christopher Kenny <ctk44@cornell.edu>
2024-02-06 17:06:31 -0500

    Update test-supported_states.R

--- tests/testthat/test-supported_states.R
+++ tests/testthat/test-supported_states.R
@@ -1,5 +1,5 @@
 with_mock_dir("t/supsts", {
   test_that("`ps_supported_states()` works", {
-    expect_s3_class(ps_model_versions(), "data.frame")
+    expect_s3_class(ps_supported_states(), "data.frame")
   })
 })
